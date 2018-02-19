87553c4bab064dd124aed636b17527095f96d152
MrCStone <carlos.l.varela92@gmail.com>
2018-02-20 00:56:49 +0100

    Got timer. Reformatted output, now it's prettier. Thing is pretty much done except for A) Auto executing script after timer is up for a bundle and B) It sometimes fails, says something about the timer not being there already and the DOM, Will have to figure that out too.

--- humble-scraper.py
+++ humble-scraper.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 
-#TODO Maybe check for time until bundle finishes, to grab new bundles on the go?
+#TODO Program Python to execute this script when time to finish the bundle is up.
 
 def get_bundle_links():
     options = Options()
@@ -25,9 +25,21 @@ def get_bundle_links():
     browser.quit()
     return list_of_urls
 
+def get_timer(url):
+    options = Options()
+    options.add_argument("--headless")
+    timer = []
+    time_left=[]
+    browser = webdriver.Firefox(firefox_options=options)
+    browser.get(url)
+    countdown = browser.find_elements_by_class_name("redesign-purchase-digit")
+    labels = browser.find_elements_by_tag_name("label")
 
-# Parse Bundles from list of urls (By now single url, the idea would be to make get_bundles function that returns a list of bundles, then parse all the urls in the list. Should add a level to the nested dict so that it gets the bundle name, the tiers and the products. Also, getting ordering okay would be amazing.)
-
+    for element, label in zip(countdown, labels):
+        timer.append(element.text+' '+label.text)
+        time_left.append(element)
+    browser.quit()
+    return timer
 
 def parse_bundles(url):
     response = requests.get(url)
@@ -35,19 +47,24 @@ def parse_bundles(url):
     tiers = soup.select(".dd-game-row")
     bundle_name = soup.title.string
     tier_list=[]
+    timer = get_timer(url)
+    time_left=", ".join(timer)
     for tier in tiers:
         if tier.select(".dd-header-headline"):
             tiername = tier.select(".dd-header-headline")[0].text.strip()
             products = tier.select(".dd-image-box-caption")
             productnames = [prodname.text.strip() for prodname in products]
             tier_list.append({"Tier": tiername, "Products": productnames})
-    return tier_list, bundle_name
+    return tier_list, bundle_name, time_left
 
-def print_it(tier_list, name):
-    print("Bundle: ", name)
+def print_it(tier_list, name, time_left):
+    print("\nBUNDLE: ", name)
+    print("\nTIME LEFT: ", time_left)
     for i in tier_list:
         print("\n")
+        print("-"*50)
         print("\tTier: ", i["Tier"])
+        print("="*50)
         print("\n")
         print("\tProducts: ")
         for product in i["Products"]:
@@ -56,9 +73,10 @@ def print_it(tier_list, name):
 
 def main():
     for bundle in get_bundle_links():
-        info, name = parse_bundles(bundle)
-        print_it(info, name)
-
+        info, name, timer = parse_bundles(bundle)
+        print("*"*50)
+        print_it(info, name, timer)
+        print("*"*50)
 
 if __name__ == '__main__':
     main()
