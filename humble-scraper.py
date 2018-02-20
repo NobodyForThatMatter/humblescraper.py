9292179c87923c4bb4d5cf018dce96b2e681974b
MrCStone <carlos.l.varela92@gmail.com>
2018-02-20 17:01:24 +0100

    Got rid of Selenium altogether, thanks to deniska on Python's IRC figured there was a better way to scrape this. No need for get timer either. Way faster now. Thank you deniska. Not much left to do, once I get the timer to work for me.

--- humble-scraper.py
+++ humble-scraper.py
@@ -2,44 +2,24 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
+import re
+import json
 
 #TODO Program Python to execute this script when time to finish the bundle is up.
 
-def get_bundle_links():
-    options = Options()
-    options.add_argument("--headless")
-    list_of_urls = []
-    browser = webdriver.Firefox(firefox_options=options)
-    browser.get("https://humblebundle.com")
-    button = browser.find_element_by_class_name("button-title")
-    button.click()
-    bundle_wrapper = browser.find_element_by_class_name("bundle-dropdown-content-wrapper")
-    bundles = bundle_wrapper.find_elements_by_class_name("more-details")
-    for bundle in bundles:
-        link = bundle.get_property("href")
-        if link not in list_of_urls:
-            list_of_urls.append(link)
-    browser.quit()
-    return list_of_urls
+def get_bundle_links_and_end():
+    list_of_bundles=[]
+    request = requests.get("http://humblebundle.com")
+    mosaic_raw = re.findall("var mosaic =(.+?);\n", request.text, re.S)
+    mosaic_loaded = json.loads(mosaic_raw[0])
+    for element in mosaic_loaded:
+        if "products" in element:
+            for product in element["products"]:
+                if product["type"] == "bundle":
+                    bundle_url="http://humblebundle.com"+product["product_url"]
+                    list_of_bundles.append({"url": bundle_url, "end": product["end_date"]})
+    return list_of_bundles
 
-def get_timer(url):
-    options = Options()
-    options.add_argument("--headless")
-    timer = []
-    time_left=[]
-    browser = webdriver.Firefox(firefox_options=options)
-    browser.get(url)
-    countdown = browser.find_elements_by_class_name("redesign-purchase-digit")
-    labels = browser.find_elements_by_tag_name("label")
-
-    for element, label in zip(countdown, labels):
-        timer.append(element.text+' '+label.text)
-        time_left.append(element)
-    browser.quit()
-    return timer
 
 def parse_bundles(url):
     response = requests.get(url)
@@ -47,24 +27,22 @@ def parse_bundles(url):
     tiers = soup.select(".dd-game-row")
     bundle_name = soup.title.string
     tier_list=[]
-    timer = get_timer(url)
-    time_left=", ".join(timer)
     for tier in tiers:
         if tier.select(".dd-header-headline"):
             tiername = tier.select(".dd-header-headline")[0].text.strip()
             products = tier.select(".dd-image-box-caption")
             productnames = [prodname.text.strip() for prodname in products]
             tier_list.append({"Tier": tiername, "Products": productnames})
-    return tier_list, bundle_name, time_left
+    return tier_list, bundle_name
 
-def print_it(tier_list, name, time_left):
+def print_it(tier_list, name, bundle_end):
     print("\nBUNDLE: ", name)
-    print("\nTIME LEFT: ", time_left)
+    print("\nBUNDLE ENDS: ", bundle_end)
     for i in tier_list:
         print("\n")
-        print("-"*50)
+        print("-"*75)
         print("\tTier: ", i["Tier"])
-        print("="*50)
+        print("="*75)
         print("\n")
         print("\tProducts: ")
         for product in i["Products"]:
@@ -72,11 +50,12 @@ def print_it(tier_list, name, time_left):
         print("\n")
 
 def main():
-    for bundle in get_bundle_links():
-        info, name, timer = parse_bundles(bundle)
-        print("*"*50)
-        print_it(info, name, timer)
-        print("*"*50)
+    for bundle in get_bundle_links_and_end():
+        bundle_end = bundle["end"]
+        info, name = parse_bundles(bundle["url"])
+        print("*"*75)
+        print_it(info,name,bundle_end)
+        print("*"*75)
 
 if __name__ == '__main__':
     main()
